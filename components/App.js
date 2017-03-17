9879642b1e71d2b800c3eb62689d1a70119f58fd
Paul Henschel <drcmda@gmail.com>
2017-03-17 16:32:56 +0100

    use transforms

--- components/App.js
+++ components/App.js
@@ -1,130 +1,81 @@
 import React from 'react';
-import Animated from 'animated/lib/targets/react-dom';
 import Parallax from './Parallax';
-
-const Cloud = ({ src, width, offset }) => {
-    const animation = new Animated.Value(1)
-    const grow = () => Animated.spring(animation, { toValue: 1.2 }).start()
-    const shrink = () => Animated.spring(animation, { toValue: 1 }).start()
-    return (
-        <Animated.img
-            style={{
-                display: 'block',
-                width: width,
-                marginLeft: offset + "%",
-                transform: [ { scale: animation } ]
-            }}
-            src={`assets/${src}.svg`}
-            onMouseOver={grow}
-            onMouseOut={shrink} />
-    )
-}
 
 export default class extends React.Component {
     render() {
-        const aws = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io';
         return (
-            <Parallax style={{ height: '400vh' }}>
+            <Parallax height="400%">
                 <Parallax.Layer
-                    depth={0.1}
+                    offset={0} speed={0.01}
                     style={{
-                        height: '500vh',
+                        height: '500%',
                         backgroundPosition: 'initial',
                         backgroundRepeat: 'repeat',
-                        backgroundColor: '#162D45',
                         backgroundImage: 'url(assets/stars.svg)'
                     }}
                 />
+
                 <Parallax.Layer
-                    depth={0.6}
+                    offset={0} speed={0.6}
                     style={{ backgroundImage: `url(assets/logo.png)`, backgroundPosition: 'center' }}
                 />
 
-                <Parallax.Layer depth={-0.2} style={{ top: '100vh' }}>
+                <Parallax.Layer offset={1} speed={-0.2}>
                     <img src="assets/satellite4.svg" style={{ width: '30%', marginLeft: '60%' }} />
                 </Parallax.Layer>
 
-                <Parallax.Layer
-                    depth={1}
-                    style={{
-                        opacity: 0.4,
-                        top: '470vh',
-                        height: '200vh',
-                        background: 'radial-gradient(circle at 50% 120%, #373737 80%, transparent 80%, transparent 100%)'
-                    }}
-                />
-                <Parallax.Layer
-                    depth={0.7}
-                    style={{
-                        opacity: 0.5,
-                        top: '460vh',
-                        height: '150vh',
-                        background: 'radial-gradient(circle at 50% 120%, #2B3E51 80%, transparent 80%, transparent 100%)'
-                    }}
-                />
-                <Parallax.Layer
-                    depth={0.6}
-                    style={{
-                        opacity: 0.6,
-                        top: '480vh',
-                        height: '125vh',
-                        background: 'radial-gradient(circle at 50% 120%, #2D4A66 80%, transparent 80%, transparent 100%)'
-                    }}
-                />
-
-                <Parallax.Layer depth={0.8} style={{ opacity: 0.2, top: '150vh' }}>
-                    <Cloud src="cloud" width={600} offset={10} />
+                <Parallax.Layer offset={1.8} speed={0.8} style={{ opacity: 0.2 }}>
+                    <img src="assets/cloud.svg" style={{ display: 'block', width: 600, marginLeft: '10%' }} />
                     <img src="assets/cloud.svg" style={{ display: 'block', width: 300, marginLeft: '40%' }} />
                     <img src="assets/cloud.svg" style={{ display: 'block', width: 100, marginLeft: '15%' }} />
                 </Parallax.Layer>
 
-                <Parallax.Layer depth={0.5} style={{ opacity: 0.4, top: '230vh' }}>
+                <Parallax.Layer offset={2.3} speed={0.5} style={{ opacity: 0.4 }}>
                     <img src="assets/cloud.svg" style={{ display: 'block', width: 200, marginLeft: '70%' }} />
                     <img src="assets/cloud.svg" style={{ display: 'block', width: 200, marginLeft: '40%' }} />
                     <img src="assets/cloud.svg" style={{ display: 'block', width: 400, marginLeft: '30%' }} />
                 </Parallax.Layer>
 
-                <Parallax.Layer depth={0.6} style={{ opacity: 0.6, top: '340vh' }}>
+                <Parallax.Layer offset={3.3} speed={0.6} style={{ opacity: 0.6 }}>
                     <img src="assets/cloud.svg" style={{ display: 'block', width: 100, marginLeft: '10%' }} />
                     <img src="assets/cloud.svg" style={{ display: 'block', width: 300, marginLeft: '40%' }} />
                     <img src="assets/cloud.svg" style={{ display: 'block', width: 400, marginLeft: '50%' }} />
                 </Parallax.Layer>
 
-                <Parallax.Layer depth={-0.1} style={{ opacity: 0.8, top: '260vh' }}>
+                <Parallax.Layer offset={2.5} speed={-0.1} style={{ opacity: 0.8 }}>
                     <img src="assets/cloud.svg" style={{ display: 'block', width: 300, marginLeft: '60%' }} />
                     <img src="assets/cloud.svg" style={{ display: 'block', width: 400, marginLeft: '20%' }} />
                     <img src="assets/cloud.svg" style={{ display: 'block', width: 200, marginLeft: '80%' }} />
                 </Parallax.Layer>
 
-                <Parallax.Layer depth={0.4} style={{ top: '470vh' }}>
+                <Parallax.Layer offset={4.8} speed={0.4}>
                     <img src="assets/cloud.svg" style={{ display: 'block', width: 300, marginLeft: '10%' }} />
                     <img src="assets/cloud.svg" style={{ display: 'block', width: 400, marginLeft: '70%' }} />
                 </Parallax.Layer>
 
                 <Parallax.Layer
-                    depth={-0.5}
+                    offset={2.3} speed={-0.4}
                     style={{
-                        top: '150vh',
-                        display: 'flex',
-                        alignItems: 'flex-end',
-                        justifyContent: 'center'
-                    }}>
-                    <img src="assets/earth.svg" style={{ width: '100%', transform: 'translate3d(0,60%,0)' }} />
-                </Parallax.Layer>
+                        backgroundSize: '100%',
+                        backgroundPosition: 'center',
+                        backgroundImage: `url(assets/earth.svg)`
+                    }} />
 
                 <Parallax.Layer
-                    depth={-0.3}
+                    offset={2.1} speed={-0.3}
                     style={{
-                        top: '180vh',
-                        display: 'flex',
-                        alignItems: 'flex-end',
-                        justifyContent: 'center'
-                    }}>
-                    <img
-                        src="assets/clients.svg"
-                        style={{ position: 'absolute', width: '70%', transform: 'translate3d(0,0,0)' }}
-                    />
-                </Parallax.Layer>
+                        backgroundSize: '80%',
+                        backgroundPosition: 'center',
+                        backgroundImage: `url(assets/clients.svg)`
+                    }} />
+
+                <Parallax.Layer
+                    offset={1.5} speed={-0.5}
+                    style={{
+                        backgroundSize: '45%',
+                        backgroundPosition: 'center',
+                        backgroundImage: `url(assets/clients-main.svg)`
+                    }} />
 
             </Parallax>
         );
--- components/Parallax.js
+++ components/Parallax.js
@@ -1,67 +1,86 @@
 import React from 'react';
 
 export default class extends React.Component {
-    constructor() {
-        super();
-        this.layers = [];
-    }
-    update() {
-        this.layers = this.refs.container.querySelectorAll("[data-type='parallax-layer']");
-    }
-    componentDidMount() {
-        this.refs.scroll.addEventListener('scroll', this.onScroll, { passive: true });
-        this.update();
-    }
-    componentDidUpdate() {
-        this.update();
-    }
-    componentWillUnmount() {
-        this.refs.scroll.removeEventListener('scroll', this.onScroll);
-    }
-
+    windowInnerHeight = 0;
+    windowPageYOffset = 0;
+    busy = false;
+    scroller = () => {
+        Object.values(this.refs).forEach(
+            layer => layer.move && layer.move(this.windowInnerHeight, this.windowPageYOffset)
+        );
+        this.busy = false;
+    };
     onScroll = event => {
-        var depth, layer, layers, topDistance, translate3d;
-        topDistance = event.target.scrollTop;
-        this.layers.forEach(layer => {
-            depth = layer.getAttribute('data-depth');
-            translate3d = 'translate3d(0, ' + (-(topDistance * depth)) + 'px, 0)';
-            layer.style['-webkit-transform'] = translate3d;
-            layer.style['-moz-transform'] = translate3d;
-            layer.style['-ms-transform'] = translate3d;
-            layer.style['-o-transform'] = translate3d;
-            layer.style.transform = translate3d;
-        });
+        if (!this.busy) {
+            this.busy = true;
+            requestAnimationFrame(this.scroller);
+            this.windowPageYOffset = window.pageYOffset;
+        }
     };
 
+    onResize = () => this.windowInnerHeight = window.innerHeight;
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.onScroll, { passive: true });
+        window.addEventListener('resize', this.onResize, false);
+        this.onResize();
+        this.onScroll();
+    }
+
+    componentDidUpdate() {
+        this.onResize();
+        this.onScroll();
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.onScroll);
+        window.removeEventListener('resize', this.onResize, false);
+    }
+
     render() {
+        this.layers = React.Children.map(this.props.children, (child, index) =>
+            React.cloneElement(child, { ...child.props, ref: `child-${index}` }));
+
         return (
             <div
-                ref="scroll"
-                style={{ width: '100%', height: '100%', overflowY: 'auto' }}
+                ref="container"
+                style={{
+                    position: 'absolute',
+                    width: '100%',
+                    transform: 'translate3d(0, 0, 0)',
+                    overflow: 'hidden',
+                    height: this.props.height
+                }}
                 className={this.props.className}>
-                <div ref="container" style={{ position: 'relative', overflow: 'hidden', ...this.props.style }}>
-                    {this.props.children}
-                </div>
+
+                {this.layers}
+
             </div>
         );
     }
 
     static Layer = class extends React.Component {
+        move(innerHeight, YOffset) {
+            let toValue = -(YOffset * this.props.speed) + innerHeight * this.props.offset;
+            if (!(this.props.style && this.props.style.height)) this.refs.layer.style.height = `${innerHeight}px`;
+            this.refs.layer.style.transform = `translate3d(0,${toValue}px,0)`;
+        }
+
         render() {
             return (
                 <div
+                    ref="layer"
                     style={{
                         position: 'absolute',
                         backgroundPosition: 'bottom center',
                         backgroundSize: 'auto',
                         backgroundRepeat: 'no-repeat',
+                        willChange: 'transform',
                         width: '100%',
-                        height: '100vh',
+                        height: this.innerHeight,
                         ...this.props.style
                     }}
-                    className={this.props.className}
-                    data-type="parallax-layer"
-                    data-depth={this.props.depth || 0}>
+                    className={this.props.className}>
                     {this.props.children}
                 </div>
             );
