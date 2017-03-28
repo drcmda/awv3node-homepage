6d427f0ee586f0c90bf195099981cc0ce3df0ef0
Paul Henschel <drcmda@gmail.com>
2017-03-28 17:48:02 +0200

    scroll off

--- components/App.js
+++ components/App.js
@@ -1,116 +1,70 @@
 import React from 'react';
-import Animated from 'animated/lib/targets/react-dom';
-import Parallax from './Parallax';
+import Parallax from 'react-springy-parallax';
 
-// Assets
-const url = (name, wrap = false) => `${wrap ? 'url(' : ''}build/assets/${name}.svg${wrap ? ')' : ''}`;
+// Little helpers ...
+const url = (name, wrap = false) => `${wrap ? 'url(' : ''}build/assets/${name}.svg${wrap ? ')' : ''}`
+const Pink = ({ children }) => <span style={{ color: '#FF6AC1' }}>{children}</span>
+const Yellow = ({ children }) => <span style={{ color: '#EFF59B' }}>{children}</span>
+const Lightblue = ({ children }) => <span style={{ color: '#9AEDFE' }}>{children}</span>
+const Green = ({ children }) => <span style={{ color: '#57EE89' }}>{children}</span>
+const Blue = ({ children }) => <span style={{ color: '#57C7FF' }}>{children}</span>
+const Gray = ({ children }) => <span style={{ color: '#909090' }}>{children}</span>
 
 export default class extends React.Component {
-    animation = new Animated.Value(1);
-    hover = () => Animated.spring(this.animation, { toValue: 1.2 }).start();
-    unhover = () => Animated.spring(this.animation, { toValue: 1 }).start();
-
     render() {
         return (
-            <Parallax pages={4}>
+            <Parallax
+                ref="parallax"
+                pages={4}
+                scrolling={false}>
+
+                <Parallax.Layer offset={1} speed={1} style={{ backgroundColor: '#243B4A' }} />
+                <Parallax.Layer offset={2} speed={1} style={{ backgroundColor: '#805E73' }} />
+                <Parallax.Layer offset={3} speed={1} style={{ backgroundColor: '#87BCDE' }} />
 
                 <Parallax.Layer
-                    offset={0}
-                    speed={0}
-                    style={{ backgroundColor: '#03101d', backgroundSize: 'cover', backgroundImage: url('stars', true) }}
+                    offset={0} speed={0} factor={4}
+                    style={{ backgroundImage: url('stars', true), backgroundSize: 'cover' }}
                 />
 
-                <Parallax.Layer offset={1} speed={0} style={{ backgroundColor: '#53466f' }} />
-
-                <Parallax.Layer offset={2} speed={0} style={{ backgroundColor: '#64d6a7' }} />
-
-                <Parallax.Layer offset={3} speed={0} style={{ backgroundColor: '#FF9696' }} />
-
-                <Parallax.Layer
-                    offset={0}
-                    speed={0.5}
-                    style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                    <img
-                        src={url('logo')}
-                        style={{ cursor: 'pointer', width: '30%' }} />
-                </Parallax.Layer>
-
-                <Parallax.Layer offset={3} speed={0} style={{ backgroundColor: 'rgburl(255,150,150,1)' }} />
-
-                <Parallax.Layer offset={1.2} speed={-0.2}>
+                <Parallax.Layer offset={1.3} speed={-0.3} style={{ pointerEvents: 'none' }}>
                     <img src={url('satellite4')} style={{ width: '15%', marginLeft: '70%' }} />
                 </Parallax.Layer>
 
-                <Parallax.Layer offset={1} speed={0.8} style={{ opacity: 0.2 }}>
-                    <img src={url('cloud')} style={{ display: 'block', width: '40%', marginLeft: '10%' }} />
-                    <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '40%' }} />
+                <Parallax.Layer offset={1} speed={0.8} style={{ opacity: 0.1 }}>
+                    <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '55%' }} />
                     <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '15%' }} />
                 </Parallax.Layer>
 
-                <Parallax.Layer offset={1.75} speed={0.5} style={{ opacity: 0.2 }}>
+                <Parallax.Layer offset={1.75} speed={0.5} style={{ opacity: 0.1 }}>
                     <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '70%' }} />
                     <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '40%' }} />
-                    <img src={url('cloud')} style={{ display: 'block', width: '30%', marginLeft: '30%' }} />
                 </Parallax.Layer>
 
-                <Parallax.Layer offset={2} speed={0.2} style={{ opacity: 0.4 }}>
+                <Parallax.Layer offset={2} speed={0.2} style={{ opacity: 0.2 }}>
                     <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '10%' }} />
-                    <img src={url('cloud')} style={{ display: 'block', width: '30%', marginLeft: '40%' }} />
-                    <img src={url('cloud')} style={{ display: 'block', width: '25%', marginLeft: '50%' }} />
+                    <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '75%' }} />
                 </Parallax.Layer>
 
-                <Parallax.Layer offset={2.7} speed={-0.1} style={{ opacity: 0.6 }}>
-                    <img src={url('cloud')} style={{ display: 'block', width: '30%', marginLeft: '60%' }} />
-                    <img src={url('cloud')} style={{ display: 'block', width: '30%', marginLeft: '20%' }} />
-                    <img src={url('cloud')} style={{ display: 'block', width: '15%', marginLeft: '80%' }} />
+                <Parallax.Layer offset={2.6} speed={-0.1} style={{ opacity: 0.4 }}>
+                    <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '60%' }} />
+                    <img src={url('cloud')} style={{ display: 'block', width: '25%', marginLeft: '30%' }} />
+                    <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '80%' }} />
                 </Parallax.Layer>
 
-                <Parallax.Layer offset={3.6} speed={0.4}>
-                    <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '10%' }} />
-                    <img src={url('cloud')} style={{ display: 'block', width: '30%', marginLeft: '70%' }} />
-                </Parallax.Layer>
-
-                <Parallax.Layer
-                    offset={1}
-                    speed={0}
-                    style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                    <Animated.img
-                        src={url('server')}
-                        style={{ cursor: 'pointer', width: '20%', transform: [{ scale: this.animation }] }}
-                        onMouseOver={this.hover}
-                        onMouseOut={this.unhover}
-                    />
+                <Parallax.Layer offset={3.6} speed={0.4} style={{ opacity: 0.6 }}>
+                    <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '5%' }} />
+                    <img src={url('cloud')} style={{ display: 'block', width: '15%', marginLeft: '75%' }} />
                 </Parallax.Layer>
 
                 <Parallax.Layer
-                    offset={2}
-                    speed={0}
-                    style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                    <Animated.img
-                        src={url('bash')}
-                        style={{ cursor: 'pointer', width: '40%', transform: [{ scale: this.animation }] }}
-                        onMouseOver={this.hover}
-                        onMouseOut={this.unhover}
-                    />
+                    offset={3.5} speed={-0.4}
+                    style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', pointerEvents: 'none' }}>
+                    <img src={url('earth')} style={{ width: '60%' }} />
                 </Parallax.Layer>
 
                 <Parallax.Layer
-                    offset={3.5}
-                    speed={0.2}
-                    style={{ opacity: 0.4, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                    <img src={url('ether')} style={{ width: '100%' }} />
-                </Parallax.Layer>
-
-                <Parallax.Layer
-                    offset={3.5}
-                    speed={-0.4}
-                    style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                    <img src={url('earth')} style={{ width: '100%' }} />
-                </Parallax.Layer>
-
-                <Parallax.Layer
-                    offset={3}
-                    speed={-0.3}
+                    offset={3} speed={-0.3}
                     style={{
                         backgroundSize: '80%',
                         backgroundPosition: 'center',
@@ -119,14 +73,59 @@ export default class extends React.Component {
                 />
 
                 <Parallax.Layer
-                    offset={3}
-                    speed={-0}
+                    offset={0} speed={0.5}
+                    style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+                    onClick={() => this.refs.parallax.scrollTo(1)}>
+                    <div
+                        style={{
+                            whiteSpace: 'pre',
+                            fontFamily: 'Menlo-Regular, Menlo, monospace',
+                            fontSize: 14,
+                            lineHeight: '10px',
+                            color: 'white'
+                        }}>
+                        <p><Gray> &gt;</Gray>  npm install awv3-node --save</p>
+                        <br />
+                        <p><Gray> 1</Gray>  <Pink>const</Pink> Server <Pink>=</Pink> <Lightblue>require</Lightblue><Gray>(</Gray><Green>'awv3-node/server'</Green><Gray>)</Gray></p>
+                        <br />
+                        <p><Gray> 2</Gray>  <Pink>new</Pink> Server<Gray>(&#123;</Gray></p>
+                        <p><Gray> 3</Gray>      <Yellow>output</Yellow><Pink>:</Pink> __dirname <Pink>+</Pink></p>
+                        <p>            <Green>'/node_modules/@awv/classcad_cadapplication'</Green><Gray>,</Gray></p>
+                        <p><Gray> 4</Gray>      <Yellow>config</Yellow><Pink>:</Pink> <Green>'CADApplication.ini'</Green><Gray>,</Gray></p>
+                        <p><Gray> 5</Gray>      <Yellow>publicport</Yellow><Pink>:</Pink> <Blue>process</Blue>.<Blue>env</Blue>.<Blue>port</Blue> <Pink>||</Pink>&nbsp;8181<Gray>,</Gray></p>
+                        <p><Gray> 6</Gray>      <Yellow>privateport</Yellow><Pink>:</Pink> 9191<Gray>,</Gray></p>
+                        <p><Gray> 7</Gray>      <Yellow>instances</Yellow><Pink>:</Pink> 3</p>
+                        <p><Gray> 8</Gray>  <Gray>})</Gray><Pink>.</Pink><Lightblue>start</Lightblue><Gray>()</Gray></p>
+                    </div>
+                </Parallax.Layer>
+
+                <Parallax.Layer
+                    offset={1} speed={0}
+                    onClick={() => this.refs.parallax.scrollTo(2)}
                     style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                    <Animated.img
+                    <img
+                        src={url('server')}
+                        style={{ width: '20%', transform: [{ scale: this.animation }] }}
+                    />
+                </Parallax.Layer>
+
+                <Parallax.Layer
+                    offset={2} speed={0}
+                    onClick={() => this.refs.parallax.scrollTo(3)}
+                    style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                    <img
+                        src={url('bash')}
+                        style={{ width: '40%', transform: [{ scale: this.animation }] }}
+                    />
+                </Parallax.Layer>
+
+                <Parallax.Layer
+                    offset={3} speed={-0}
+                    style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+                    onClick={() => this.refs.parallax.scrollTo(0)}>
+                    <img
                         src={url('clients-main')}
-                        style={{ cursor: 'pointer', width: '40%', transform: [{ scale: this.animation }] }}
-                        onMouseOver={this.hover}
-                        onMouseOut={this.unhover}
+                        style={{ width: '40%', transform: [{ scale: this.animation }] }}
                     />
                 </Parallax.Layer>
 
--- components/Parallax.js
+++ components/Parallax.js
@@ -1,69 +1,110 @@
-import React from 'react';
-import Animated from 'animated/lib/targets/react-dom';
+import React from 'react'
+import Animated from 'animated/lib/targets/react-dom'
 
 export default class extends React.Component {
-    state = { ready: false };
+    static propTypes = {
+        pages: React.PropTypes.number.isRequired,
+        effect: React.PropTypes.func,
+        scrolling: React.PropTypes.bool
+    }
+    static defaultProps = {
+        effect: (animation, toValue) => Animated.spring(animation, { toValue }),
+        scrolling: true
+    }
+    static childContextTypes = { parallax: React.PropTypes.object }
 
-    layers = [];
-    height = 0;
-    scrollTop = 0;
-    busy = false;
+    constructor(props) {
+        super(props)
+        this.state = { ready: false }
+        this.layers = []
+        this.height = 0
+        this.scrollTop = 0
+        this.offset = 0
+        this.busy = false
+    }
 
-    scroller = () => {
-        this.layers.forEach(layer => layer.move(this.height, this.scrollTop, this.props.pages));
-        this.busy = false;
-    };
-    scrollerRaf = () => requestAnimationFrame(this.scroller);
+    moveItems = () => {
+        this.layers.forEach(layer => layer.setPosition(this.height, this.scrollTop))
+        this.busy = false
+    }
+
+    scrollerRaf = () => requestAnimationFrame(this.moveItems)
 
     onScroll = event => {
         if (!this.busy) {
-            this.busy = true;
-            this.scrollerRaf();
-            this.scrollTop = event.target.scrollTop;
+            this.busy = true
+            this.scrollerRaf()
+            this.scrollTop = event.target.scrollTop
         }
-    };
+    }
 
-    onResize = () => {
-        this.scrollTop = this.refs.container.scrollTop;
-        this.height = this.refs.container.clientHeight;
-        if (this.refs.content) this.refs.content.style.height = `${this.height * this.props.pages}px`;
-        this.layers.forEach(layer => layer.height(this.height));
-        this.scroller();
-    };
+    update = () => {
+        this.height = this.refs.container.clientHeight
 
-    componentDidUpdate() {
-        this.layers = Object.keys(this.refs).filter(key => this.refs[key].move).map(key => this.refs[key]);
-        this.onResize();
+        if (this.props.scrolling) this.scrollTop = this.refs.container.scrollTop
+        else this.refs.container.scrollTop = (this.scrollTop = this.offset * this.height)
+
+        if (this.refs.content) this.refs.content.style.height = `${this.height * this.props.pages}px`
+        this.layers.forEach(layer => {
+            layer.setHeight(this.height, true)
+            layer.setPosition(this.height, this.scrollTop, true)
+        })
+    }
+
+    updateRaf = () => {
+        requestAnimationFrame(this.update)
+        // Some browsers don't fire on maximize
+        setTimeout(this.update, 150)
+    }
+
+    scrollStop = event => this.animatedScroll && this.animatedScroll.stopAnimation()
+
+    scrollTo(offset) {
+        this.scrollStop()
+        this.offset = offset
+        const target = this.refs.container
+        this.animatedScroll = new Animated.Value(target.scrollTop)
+        this.animatedScroll.addListener(({ value }) => target.scrollTop = value)
+        this.props.effect(this.animatedScroll, offset * this.height).start()
+    }
+
+    getChildContext() {
+        return { parallax: this }
     }
 
     componentDidMount() {
-        window.addEventListener('resize', this.onResize, false);
-        this.componentDidUpdate();
-        this.setState({ ready: true });
+        window.addEventListener('resize', this.updateRaf, false)
+        this.update()
+        this.setState({ ready: true })
     }
 
     componentWillUnmount() {
-        window.removeEventListener('resize', this.onResize, false);
+        window.removeEventListener('resize', this.updateRaf, false)
+    }
+
+    componentDidUpdate() {
+        this.update()
     }
 
     render() {
-        this.layers = React.Children.map(this.props.children, (child, index) =>
-            React.cloneElement(child, { ...child.props, ref: `child-${index}`, container: this }));
+        const { style, innerStyle, pages, className, scrolling, children } = this.props
         return (
             <div
                 ref="container"
                 onScroll={this.onScroll}
+                onWheel={scrolling && this.scrollStop}
+                onTouchStart={scrolling && this.scrollStop}
                 style={{
                     position: 'absolute',
                     width: '100%',
                     height: '100%',
-                    overflow: 'scroll',
+                    overflow: scrolling ? 'scroll' : 'hidden',
                     overflowX: 'hidden',
                     WebkitOverflowScrolling: 'touch',
                     transform: 'translate3d(0, 0, 0)',
-                    ...this.props.style
+                    ...style
                 }}
-                className={this.props.className}>
+                className={className}>
 
                 {this.state.ready &&
                     <div
@@ -73,49 +114,71 @@ export default class extends React.Component {
                             width: '100%',
                             transform: 'translate3d(0, 0, 0)',
                             overflow: 'hidden',
-                            height: this.height * this.props.pages,
-                            ...this.props.innerStyle
+                            height: this.height * pages,
+                            ...innerStyle
                         }}>
-                        {this.layers}
+                        {children}
                     </div>}
 
             </div>
-        );
+        )
     }
 
     static Layer = class extends React.Component {
-        constructor(props) {
-            super(props);
-            const targetScroll = Math.floor(props.offset) * props.container.height;
-            const offset = props.container.height * props.offset + targetScroll * props.speed;
-            const toValue = parseFloat(-(props.container.scrollTop * props.speed) + offset);
-            this.animTranslate = new Animated.Value(toValue);
-            const height = props.container.height * props.factor;
-            this.animHeight = new Animated.Value(height);
-        }
-
+        static contextTypes = { parallax: React.PropTypes.object }
         static propTypes = {
             factor: React.PropTypes.number,
             offset: React.PropTypes.number,
-            stretch: React.PropTypes.number
-        };
-        static defaultProps = { factor: 1, offset: 0, stretch: 1 };
-
-        move(height, scrollTop, pages) {
-            const targetScroll = Math.floor(this.props.offset) * height;
-            const offset = height * this.props.offset + targetScroll * this.props.speed;
-            const toValue = parseFloat(-(scrollTop * this.props.speed) + offset);
-            Animated.spring(this.animTranslate, { toValue }).start();
+            speed: React.PropTypes.number
+        }
+        static defaultProps = {
+            factor: 1,
+            offset: 0,
+            speed: 0
         }
 
-        height(height) {
-            const toValue = parseFloat(height * this.props.factor);
-            Animated.spring(this.animHeight, { toValue }).start();
+        constructor(props, context) {
+            super(props, context)
+            const parallax = context.parallax
+            const targetScroll = Math.floor(props.offset) * parallax.height
+            const offset = parallax.height * props.offset + targetScroll * props.speed
+            const toValue = parseFloat(-(parallax.scrollTop * props.speed) + offset)
+            this.animatedTranslate = new Animated.Value(toValue)
+            const height = parallax.height * props.factor
+            this.animatedHeight = new Animated.Value(height)
+        }
+
+        componentDidMount() {
+            const parent = this.context.parallax
+            parent.layers = parent.layers.concat(this)
+            parent.update()
+        }
+
+        componentWillUnmount() {
+            const parent = this.context.parallax
+            parent.layers = parent.layers.filter(layer => layer !== this)
+            parent.update()
+        }
+
+        setPosition(height, scrollTop, immediate = false) {
+            const targetScroll = Math.floor(this.props.offset) * height
+            const offset = height * this.props.offset + targetScroll * this.props.speed
+            const toValue = parseFloat(-(scrollTop * this.props.speed) + offset)
+            if (!immediate) this.context.parallax.props.effect(this.animatedTranslate, toValue).start()
+            else this.animatedTranslate.setValue(toValue)
+        }
+
+        setHeight(height, immediate = false) {
+            const toValue = parseFloat(height * this.props.factor)
+            if (!immediate) this.context.parallax.props.effect(this.animatedHeight, toValue).start()
+            else this.animatedHeight.setValue(toValue)
         }
 
         render() {
+            const { style, children, offset, speed, factor, ...props } = this.props
             return (
                 <Animated.div
+                    {...props}
                     ref="layer"
                     style={{
                         position: 'absolute',
@@ -123,21 +186,20 @@ export default class extends React.Component {
                         backgroundRepeat: 'no-repeat',
                         willChange: 'transform',
                         width: '100%',
-                        height: this.animHeight,
+                        height: this.animatedHeight,
                         transform: [
                             {
-                                translate3d: this.animTranslate.interpolate({
-                                    inputRange: [0, 100000],
-                                    outputRange: ['0,0px,0', '0,100000px,0']
+                                translate3d: this.animatedTranslate.interpolate({
+                                    inputRange: [0, 1],
+                                    outputRange: ['0,0px,0', '0,1px,0']
                                 })
                             }
                         ],
-                        ...this.props.style
-                    }}
-                    className={this.props.className}>
-                    {this.props.children}
+                        ...style
+                    }}>
+                    {children}
                 </Animated.div>
-            );
+            )
         }
-    };
+    }
 }
